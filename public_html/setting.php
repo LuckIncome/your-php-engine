f9b4c89b17874c7598dff63797ac037673fafaf3
Luckincome <artas_lider_ordena@mail.ru>
2020-07-27 21:43:17 +0600

    php engine

---
+++ public_html/setting.php
@@ -0,0 +1,19 @@
+<?php
+define ('HOST', '');
+define ('USER', '');
+define ('PASS', '');
+define('DB', '');
+
+define('ADMIN_PASS', '');
+define('ADMIN_IP', '');
+
+define('QIWI_PHONE', '');
+define('QIWI_PASS', '');
+
+
+
+
+define('ADMIN_PASS', '');
+define('YANDEX_ID', '');
+define('YANDEX_TOKEN', '');
+?>
